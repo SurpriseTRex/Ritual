67cf97beea27aab031abe5ee7ef5efdba90b7e5d
Kiblinix <JackAMoore94@gmail.com>
2016-01-29 21:15:01 +0000

    Added draggable object

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 
 public class Draggable : MonoBehaviour 
 {
+	Rigidbody2D grabbedObject;
+    
+    void Update ()
+    {
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 dir = Vector3.zero;
 
-    void Start () 
-    {
-    
+			RaycastHit2D hit = Physics2D.Raycast (mousePos, dir);
+			if (hit != null && hit.collider.gameObject.tag == "Draggable")
+			{
+				grabbedObject = hit.collider.attachedRigidbody;
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0))
+		{
+			grabbedObject = null;
+		}
     }
-    
-    void Update () 
-    {
-    
-    }
+
+	void FixedUpdate()
+	{
+		if (grabbedObject != null) 
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			grabbedObject.position = mousePos;
+		}
+	}
 }
