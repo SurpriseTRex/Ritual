fd678e9f8bd28864a6520767e9a0dea62f39b81d
Sean Johnston <sean.c.johnston@gmail.com>
2016-01-29 20:31:38 +0000

    add knife resource

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Draggable : MonoBehaviour 
+{
+
+    void Start () 
+    {
+    
+    }
+    
+    void Update () 
+    {
+    
+    }
+}
